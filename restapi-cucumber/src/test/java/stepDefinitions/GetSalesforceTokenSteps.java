0975a8a51a25a1cbd6afd30d2ff65c8c49e08a7c
Nag <nag324@gmail.com>
2020-01-21 22:55:05 +1300

    Added feature file and stepdefinition

---
+++ restapi-cucumber/src/test/java/stepDefinitions/GetSalesforceTokenSteps.java
@@ -0,0 +1,26 @@
+package stepDefinitions;
+
+
+import io.cucumber.java.PendingException;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class GetSalesforceTokenSteps {
+
+    @Given("^I perform authentication for \"([^\"]*)\" with params$")
+    public void i_perform_authentication_for_something_with_params(String strArg1) throws Throwable {
+        throw new PendingException();
+    }
+
+    @Given("^I perform GET operation for \"([^\"]*)\"$")
+    public void i_perform_get_operation_for_something(String strArg1) throws Throwable {
+        throw new PendingException();
+    }
+
+    @Then("^I should see label name as Account$")
+    public void i_should_see_label_name_as_account() throws Throwable {
+        throw new PendingException();
+    }
+
+}
